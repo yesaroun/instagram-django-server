3e00b467172ec63869c00cc2ab525fa6d977d478
yesaroun <akor1@naver.com>
2023-02-21 14:42:13 +0900

    feat: admin 부분 생성

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Feed
 
-# Register your models here.
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = (
+        "caption",
+        "likesNum",
+        "reviewsNum",
+    )
