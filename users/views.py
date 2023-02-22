e14046b8aaad322202cbb3940e16aa76be36a5a3
yesaroun <akor1@naver.com>
2023-02-22 14:11:09 +0900

    fix: 여러 user get으로 수정

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import UserSerializer
+from .models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -11,12 +12,17 @@ class MyInfo(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):  # Read
-        user = request.user
-
-        # Serialize화 해줘야 -> 유저에게 보낼 수 있다.
-        serializer = UserSerializer(user)
-
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+        # 수업 방식 단 이 방법으로는 하나의 데이터만 받아와짐
+        # user = request.user       # 이러면 python이 알아서 찾아주는 듯
+        #
+        # # Serialize화 해줘야 -> 유저에게 보낼 수 있다.
+        # serializer = UserSerializer(user)
+        #
+        # return Response(serializer.data)
 
     def post(self):  # Create
         pass
