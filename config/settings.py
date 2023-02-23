3696a1c3bec5ac0461f0df6d98a838068a4a47ae
yesaroun <akor1@naver.com>
2023-02-23 13:55:02 +0900

    feat: cors 처리

--- config/settings.py
+++ config/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     "reviews.apps.ReviewsConfig",
     "users.apps.UsersConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +124,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"  # 내가 user 모델을 만들겠다
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000/"]
+
+CORS_ALLOW_CREDENTIALS = True
--- feeds/views.py
+++ feeds/views.py
@@ -17,6 +17,8 @@ from rest_framework.response import Response
 
 
 class AllFeeds(APIView):
+    """전체 게시글을 불러오는 api"""
+
     def get(self, request):
         all_feeds = Feed.objects.all()
         serializer = FeedSerializer(all_feeds, many=True)
@@ -24,6 +26,8 @@ class AllFeeds(APIView):
 
 
 class FeedDetail(APIView):
+    """특정 feed의 상세 내용을 불러오는 api"""
+
     def get_object(self, feed_id):
         try:
             return Feed.objects.get(id=feed_id)
@@ -37,14 +41,18 @@ class FeedDetail(APIView):
 
 
 class UserFeeds(APIView):
+    """username을 받아서 user의 게시글을 불러오는 api"""
+
     def get_objects(self, username):
         try:
             user_id = User.objects.get(username=username).id
+            print("user_id", user_id)
             return Feed.objects.filter(user=user_id)
         except Feed.DoesNotExist:
             raise NotFound
 
     def get(self, request, username):
         feeds = self.get_objects(username)
+        print("user_feeds", username)
         serializer = UserFeedsSerializer(feeds, many=True)
         return Response(serializer.data)
