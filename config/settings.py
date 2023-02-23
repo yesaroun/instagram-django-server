de893faa9df327d68213c30ed6b3db1fae7a42b5
yesaroun <akor1@naver.com>
2023-02-23 14:52:28 +0900

    feat: api 통신 라이브러리 추가

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"  # 내가 user 모델을 만들겠다
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000/"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
