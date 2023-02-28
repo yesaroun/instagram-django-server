611dc66a00e041be502207ce4295f8fcf1614032
yesaroun <akor1@naver.com>
2023-02-28 10:31:17 +0900

    feat: django session을 통한 로그인 처리

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import MyInfo
+from .views import MyInfo, Login, Logout
 
-urlpatterns = [path("myinfo/", MyInfo.as_view())]
+urlpatterns = [
+    path("myinfo/", MyInfo.as_view()),
+    path("login", Login.as_view()),
+    path("logout", Logout.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from rest_framework.permissions import IsAuthenticated
-
-from .serializers import UserSerializer
-from .models import User
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
+from rest_framework import status
+from .serializers import UserSerializer
+from .models import User
+from django.contrib.auth import authenticate, login
 
 
 class MyInfo(APIView):
@@ -31,3 +32,34 @@ class MyInfo(APIView):
         pass
 
     # def delete(self):  # Delete
+
+
+class Login(APIView):
+    """django의 seesion을 활용한 로그인"""
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            raise ParseError()
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        print(user)
+
+        if user:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+
+class Logout(APIView):
+    """django의 session을 활용한 로그아웃"""
+
+    def post(self, request):
+        pass
